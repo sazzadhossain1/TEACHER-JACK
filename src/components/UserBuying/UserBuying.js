a8d3742d1592c1b9ac6d9ac9cd6773e5cc18637c
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-11-07 16:35:47 +0600

    user selse

--- src/components/UserBuying/UserBuying.js
+++ src/components/UserBuying/UserBuying.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import "./UserBuying.css";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import UseBuyingDetails from "../UseBuyingDetails/UseBuyingDetails";
 
 const UserBuying = () => {
@@ -14,22 +14,33 @@ const UserBuying = () => {
       <table className="table-border table" cellPadding={30}>
         <thead>
           <tr className="t_head">
-            <th>Name</th>
+            <th>Product Name</th>
             <th>Payee Number</th>
-            <th>pay Method</th>
+            <th>Pay Method</th>
             <th>Price</th>
-            <th>Download</th>
+            <th>discount</th>
+            <th>Download Avle</th>
             <th>Status</th>
           </tr>
         </thead>
         <tbody>
           {userBuyingGetApi.data.map((data) => (
             <tr className="table_colum" key={data.id}>
-              <td>{data.name}</td>
+              <td>{data.product_name}</td>
               <td>{data.payee_number}</td>
               <td>{data.pay_method}</td>
               <td>{data.price}</td>
-              <td>{data.download}</td>
+              <td>{data.discount}</td>
+
+              <td>
+                {data.url ? (
+                  <Link target="_blank" className="btn" to={data.url}>
+                    Download
+                  </Link>
+                ) : (
+                  ""
+                )}
+              </td>
               <td>{data.status}</td>
             </tr>
           ))}
--- src/components/UserSales/UserSales.js
+++ src/components/UserSales/UserSales.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./UserSales.css";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import UserSalesDetails from "../UserSalesDetails/UserSalesDetails";
 
 const UserSales = () => {
@@ -11,13 +11,58 @@ const UserSales = () => {
     return <div>Loading...</div>; // You can show a loading indicator or message while data is being fetched
   }
   return (
-    <div>
-      <h1>User Sales</h1>
-      {userSalesGetApi.data.map((data) => (
-        <UserSalesDetails key={data.id} data={data}></UserSalesDetails>
-      ))}
+    <div className="useBuyingDetails_parent_div mt-10">
+      <table className="table-border table" cellPadding={30}>
+        <thead>
+          <tr className="t_head">
+            <th>Product Name</th>
+            <th>Payee Number</th>
+            <th>Pay Method</th>
+            <th>Price</th>
+            <th>discount</th>
+            <th>Download Avle</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {userSalesGetApi.data.map((data) => (
+            <tr className="table_colum" key={data.id}>
+              <td>{data.id}</td>
+              <td>{data.payee_number}</td>
+              <td>{data.pay_method}</td>
+              <td>{data.price}</td>
+              <td>{data.discount}</td>
+              <td>
+                {data.url ? (
+                  <Link target="_blank" className="btn" to={data.url}>
+                    Download
+                  </Link>
+                ) : (
+                  ""
+                )}
+              </td>
+
+              <td>{data.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
 
 export default UserSales;
+// created_at,
+//     discount,
+//     download,
+//     pay_method,
+//     payee_number,
+//     price,
+//     ref_discount,
+//     seller_id,
+//     status,
+//     trx_id,
+//     updated_at,
+//     url,
+//     user_id,
